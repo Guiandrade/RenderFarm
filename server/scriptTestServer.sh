78456ca6d3b36e9c400715198e1a99bc6317d65d
Tiago Martins <tiagomousinhomartins@gmail.com>
2017-04-17 17:47:44 +0100

    Update scriptTestServer.sh

--- server/scriptTestServer.sh
+++ server/scriptTestServer.sh
@@ -3,7 +3,7 @@
 
 a=0
 
-modelFilename="test05.bmp"
+modelFilename="test05"
 scene_columns=1000
 scene_rows=500
 window_columns=1000
@@ -13,5 +13,5 @@ row_offset=50
 
 for a in 0 1 2 3 4 5
 do
-	( a="$((a +1))" && curl -X GET "cnv-lab-aws-lb-1328451237.eu-west-1.elb.amazonaws.com/?f="$modelFilename"&sc="$scene_columns"&sr="$scene_rows"&wc="$window_columns"&wr="$window_rows"&coff="$column_offset"&roff="$row_offset"" ) &
+	( a="$((a +1))" && curl -X GET "cnv-lab-aws-lb-1328451237.eu-west-1.elb.amazonaws.com/r.html?f="$modelFilename"&sc="$scene_columns"&sr="$scene_rows"&wc="$window_columns"&wr="$window_rows"&coff="$column_offset"&roff="$row_offset"" ) &
 done 
